0ca70da47188c3aff91db338e371fb98ae4b0137
rafaelsouzaribeiro <rafaelribeirosouza86@gmail.com>
2026-06-16 20:30:29 -0300

    adding the tag Async to events

--- src/main/kotlin/mercado_livro/events/listener/UpdateSoldBookListener.kt
+++ src/main/kotlin/mercado_livro/events/listener/UpdateSoldBookListener.kt
@@ -2,11 +2,9 @@ package mercado_livro.events.listener
 
 import mercado_livro.events.PurchaseEvent
 import mercado_livro.service.BookService
-import mercado_livro.service.PurchaseService
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
 class UpdateSoldBookListener(

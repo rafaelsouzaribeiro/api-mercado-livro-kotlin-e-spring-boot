8fcd537ce1d929186bbc564546d348ee6daa9385
rafaelsouzaribeiro <rafaelribeirosouza86@gmail.com>
2026-06-17 21:12:09 -0300

    adding request matchers

--- src/main/kotlin/mercado_livro/config/SecurityConfig.kt
+++ src/main/kotlin/mercado_livro/config/SecurityConfig.kt
@@ -2,12 +2,38 @@ package mercado_livro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
+    private val publicPostMatchers = arrayOf(
+        "/customer"
+    )
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder{
         return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors(Customizer.withDefaults())
+            .csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers(HttpMethod.POST, *publicPostMatchers).permitAll()
+                auth.anyRequest().authenticated()
+            }
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+
+        return http.build()
     }
 }

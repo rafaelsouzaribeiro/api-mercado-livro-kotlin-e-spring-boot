1f0e50cb0ec345e6c428844cdc3fff3a841519ad
rafaelsouzaribeiro <rafaelribeirosouza86@gmail.com>
2026-06-30 20:14:43 -0300

    add and fix tests

--- src/test/kotlin/mercado_livro/controller/CustomerControllerTest.kt
+++ src/test/kotlin/mercado_livro/controller/CustomerControllerTest.kt
@@ -1,23 +1,31 @@
 package mercado_livro.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import mercado_livro.controller.request.PostCustomerRequest
 import mercado_livro.enums.Roles
 import mercado_livro.helper.Token
 import mercado_livro.helper.buildCustomers
 import mercado_livro.repository.CustomerRepository
+import mercado_livro.security.UserCustomDetails
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.UUID
+import kotlin.random.Random
+import kotlin.test.assertEquals
 
 
 @SpringBootTest
@@ -43,76 +51,109 @@ class CustomerControllerTest {
     @AfterEach
     fun tearDown() = customerRepository.deleteAll()
 
-    @Nested
-    inner class Customer {
-        private fun getToken(): String {
-            val passwordNormal = "password123"
+    @Test
+    fun `should return all customer`() {
+        val user1 = buildCustomers(
+            email = "user1@email.com",
+            roles = setOf(Roles.ADMIN)
+        )
 
-            val passwordEncrypt =
-                org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().encode(passwordNormal)
+        val user2 = buildCustomers(
+            email = "user2@email.com",
+        )
 
-            val adminUser = buildCustomers(
-                email = "admin@email.com",
-                password = passwordEncrypt,
-                roles = setOf(Roles.ADMIN)
-            )
+        customerRepository.save(user1)
+        customerRepository.save(user2)
+        mockMvc.perform(
+            get("/customer").with(user(UserCustomDetails(user1)))
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$[?(@.email == 'user1@email.com')].id").value(user1.id))
+            .andExpect(jsonPath("$[?(@.email == 'user1@email.com')].name").value(user1.name))
+            .andExpect(jsonPath("$[?(@.email == 'user1@email.com')].status").value(user1.status!!.name))
+            .andExpect(jsonPath("$[?(@.email == 'user2@email.com')].id").value(user2.id))
+            .andExpect(jsonPath("$[?(@.email == 'user2@email.com')].name").value(user2.name))
+            .andExpect(jsonPath("$[?(@.email == 'user2@email.com')].status").value(user2.status!!.name))
+    }
 
-            customerRepository.save(adminUser)
+    @Test
+    fun `should return when name customer`() {
+        val user1 = buildCustomers(
+            name = "Gustavo",
+            roles = setOf(Roles.ADMIN)
+        )
 
-            val userLogin = adminUser.copy(password = passwordNormal)
-            return tokenHelper.getAccessToken(userLogin)
-        }
+        val user2 = buildCustomers(
+            name = "Daniel",
+            roles = setOf(Roles.ADMIN)
+        )
 
-        @Test
-        fun `should return all customer`() {
-            val token = getToken()
-            val user1 = buildCustomers(
-                email = "user1@email.com",
-            )
+        customerRepository.save(user1)
+        customerRepository.save(user2)
+        mockMvc.perform(
+            get("/customer?name=Gu").with(user(UserCustomDetails(user1)))
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.length()").value(1))
+            .andExpect(jsonPath("$[0].id").value(user1.id))
+            .andExpect(jsonPath("$[0].name").value(user1.name))
+            .andExpect(jsonPath("$[0].email").value(user1.email))
+            .andExpect(jsonPath("$[0].status").value(user1.status!!.name))
+    }
 
-            val user2 = buildCustomers(
-                email = "user2@email.com",
-            )
+    @Test
+    fun `should create customer`() {
+        val request = PostCustomerRequest("fake name","${Random.nextInt()}@fake.com","1234")
 
-            customerRepository.save(user1)
-            customerRepository.save(user2)
-            mockMvc.perform(
-                get("/customer")
-                    .header("Authorization", token)
-            )
-                .andExpect(status().isOk)
-                .andExpect(jsonPath("$[?(@.email == 'user1@email.com')].id").value(user1.id))
-                .andExpect(jsonPath("$[?(@.email == 'user1@email.com')].name").value(user1.name))
-                .andExpect(jsonPath("$[?(@.email == 'user1@email.com')].status").value(user1.status!!.name))
-                .andExpect(jsonPath("$[?(@.email == 'user2@email.com')].id").value(user2.id))
-                .andExpect(jsonPath("$[?(@.email == 'user2@email.com')].name").value(user2.name))
-                .andExpect(jsonPath("$[?(@.email == 'user2@email.com')].status").value(user2.status!!.name))
-        }
+        mockMvc.perform(
+            post("/customer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        )
+            .andExpect(status().isCreated)
 
-        @Test
-        fun `should return when name customer`() {
-            val token = getToken()
-            val user1 = buildCustomers(
-                name = "Gustavo"
-            )
+        val customers = customerRepository.findAll().toList()
+        assertEquals(1,customers.size)
+        assertEquals(request.name,customers[0].name)
+        assertEquals(request.email,customers[0].email)
 
-            val user2 = buildCustomers(
-                name = "Daniel"
-            )
+    }
 
-            customerRepository.save(user1)
-            customerRepository.save(user2)
-            mockMvc.perform(
-                get("/customer?name=Gu")
-                    .header("Authorization", token)
-            )
-                .andExpect(status().isOk)
-                .andExpect(jsonPath("$.length()").value(1))
-                .andExpect(jsonPath("$[0].id").value(user1.id))
-                .andExpect(jsonPath("$[0].name").value(user1.name))
-                .andExpect(jsonPath("$[0].email").value(user1.email))
-                .andExpect(jsonPath("$[0].status").value(user1.status!!.name))
-        }
+    @Test
+    fun `should get user by id when user has the same id`() {
+        val user1 = buildCustomers(
+            name = "Gustavo",
+            roles = setOf(Roles.ADMIN)
+        )
+
+        customerRepository.save(user1)
+        mockMvc.perform(
+            get("/customer/${user1.id}").with(user(UserCustomDetails(user1)))
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.id").value(user1.id))
+            .andExpect(jsonPath("$.name").value(user1.name))
+            .andExpect(jsonPath("$.email").value(user1.email))
+            .andExpect(jsonPath("$.status").value(user1.status!!.name))
+    }
+
+    @Test
+    fun `should return is not found when user has different id`() {
+        val id = UUID.randomUUID().toString()
+        val user1 = buildCustomers(
+            name = "Gustavo",
+            roles = setOf(Roles.ADMIN)
+        )
+
+        customerRepository.save(user1)
+        mockMvc.perform(
+            get("/customer/$id").with(user(UserCustomDetails(user1)))
+        )
+            .andExpect(status().isNotFound)
+            .andExpect(jsonPath("$.httpCode").value(404))
+            .andExpect(jsonPath("$.message").value("Customer {$id} not exists"))
+            .andExpect(jsonPath("$.internalCode").value("ML_201"))
+
     }
 }
 
